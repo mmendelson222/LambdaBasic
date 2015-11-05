c718e4560a72e55b3b3a0ae53f5dca137484b735
Michael Mendelson <mmendelson@gmail.com>
2015-11-05 14:21:31 -0500

    Initial commit

---
+++ LambdaBasic/app.js
@@ -0,0 +1,19 @@
+﻿console.log('Loading');
+var async = require('async');
+
+exports.handler = function (event, context) {
+    async.series([
+        function (callback) {
+            if (event != null) {
+                console.log('event = ' + JSON.stringify(event));
+            }
+            else {
+                console.log('No event object');
+            }
+            callback();
+        }
+    ], 
+    function (err) {
+        context.succeed("finished " + (new Date()));
+    });
+};
